c0cb968b28c9ce11322463e820a83e92cfd8165f
Valera <56437318+Fridi7@users.noreply.github.com>
2019-12-14 17:45:27 +0700

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+apt install python3-pip
+pip3 install APScheduler==3.6.3
+pip3 install grpcio==1.25.0
+pip3 install grpcio-tools==1.25.0
+pip3 install protobuf==3.10.0
+pip3 install pymongo==3.9.0
+pip3 install requests==2.22.0
+pip3 install urllib3==1.25.7
